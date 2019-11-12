aff1dd453a22c7eafef3948adc889f896ff493ad
Doni Setiawan <donisetiawan.050@gmail.com>
2019-11-12 18:36:53 +0700

    CRUD operations basic

--- routes/web.php
+++ routes/web.php
@@ -47,3 +47,56 @@ Route::get('update_books_table_2', function () {
         $table->foreign('author_id')->references('id')->on('authors');
     });
 });
+
+Route::get('book_create', function () {
+    $book              = new \App\Book;
+    $book->title       = 'My First Book!';
+    $book->pages_count = 230;
+    $book->price       = 10.5;
+    $book->description = 'A very original lorem ipsum dolor sit amet...';
+    $book->save();
+    $book              = new \App\Book;
+    $book->title       = 'My Second Book!';
+    $book->pages_count = 122;
+    $book->price       = 9.5;
+    $book->description = 'Another very original lorem ipsum dolor sit amet...';
+    $book->save();
+});
+
+Route::get('book_get_all', function () {
+    return \App\Book::all();
+});
+Route::get('book_get_2', function () {
+    return \App\Book::find(2);
+});
+Route::get('book_get_where', function () {
+    $result = \App\Book::where('pages_count', '<', 1000)->get();
+    return $result;
+});
+Route::get('book_get_where_chained', function () {
+    $result = \App\Book::where('pages_count', '<', 1000)
+        ->where('title', '=', 'My First Book!')
+        ->get();
+    return $result;
+});
+Route::get('book_get_where_iterate', function () {
+    $results = \App\Book::where('pages_count', '<', 1000)->get();
+    if (count($results) > 0) {
+        foreach ($results as $book) {
+            echo 'Book: ' . $book->title . ' - Pages:' . $book->pages_count . ' <br/>';
+        }
+    } else
+        echo 'No Results!';
+    return '';
+});
+
+Route::get('book_update', function () {
+    $book              = \App\Book::find(1);
+    $book->title       = 'My Updated First Book!';
+    $book->pages_count = 150;
+    $book->save();
+});
+
+Route::get('book_delete_1', function () {
+    \App\Book::find(1)->delete();
+});
