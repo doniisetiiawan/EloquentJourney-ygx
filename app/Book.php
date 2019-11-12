a8e945b88984a7d2034d78d3680572befa44f845
Doni Setiawan <donisetiawan.050@gmail.com>
2019-11-12 19:42:25 +0700

    Query scope

--- app/Book.php
+++ app/Book.php
@@ -6,5 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    public function scopeCheapButBig($query)
+    {
+        return $query->where('price', '<', 10)->where('pages_count', '>', 300);
+    }
+
+    public function scopeCheap($query)
+    {
+        return $query->where('price', '<', 10);
+    }
+
+    public function scopeExpensive($query)
+    {
+        return $query->where('price', '>', 100);
+    }
+
+    public function scopeLong($query)
+    {
+        return $query->where('pages_count', '>', 700);
+    }
+
+    public function scopeShort($query)
+    {
+        return $query->where('pages_count', '<', 100);
+    }
+
+    public function scopeLongAndCheaperThan($query, $amount)
+    {
+        return $query->long()->where('price', '<', $amount);
+    }
 }
