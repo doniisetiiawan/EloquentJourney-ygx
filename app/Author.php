a1c7f47641b45b10a3eb13288120d186d0651785
Doni Setiawan <donisetiawan.050@gmail.com>
2019-11-14 16:12:32 +0700

    Self-validation feature

--- app/Author.php
+++ app/Author.php
@@ -2,10 +2,32 @@
 
 namespace App;
 
+use App\Aweloquent\AweloquentModel;
 use Illuminate\Database\Eloquent\Model;
 
-class Author extends Model
+class Author extends AweloquentModel
 {
+    protected $fillable = [
+        'first_name', 'last_name', 'bio'
+    ];
+
+    protected static $rules = [
+        'everytime' => [
+            'first_name' => 'required'
+        ],
+        'create'    => [
+            'last_name' => 'required'
+        ],
+        'update'    => [
+            'bio' => 'required'
+        ],
+    ];
+
+    protected static $messages = [
+        'first_name.required' => 'You forgot the first name!',
+        'last_name.required'  => 'You forgot the last name!'
+    ];
+
     public function books()
     {
         return $this->hasMany(Book::class);
--- app/Aweloquent/AweloquentModel.php
+++ app/Aweloquent/AweloquentModel.php
@@ -7,10 +7,48 @@ use Illuminate\Http\Request;
 
 class AweloquentModel extends Model
 {
+    protected static $rules    = [];
+    protected static $messages = [];
+
+    public $errors;
+
     public function __construct(array $attributes = [])
     {
         $attributes = $this->autoHydrate($attributes);
         parent::__construct($attributes);
+    }
+
+    public function validate()
+    {
+        static::$rules = $this->mergeValidationRules();
+        $validator     = \Validator::make($this->attributes, static::$rules, static::$messages);
+        if ($validator->fails()) {
+            $this->errors = $validator->messages();
+            return false;
+        }
+        return true;
+    }
+
+    private function mergeValidationRules()
+    {
+        if ($this->exists)
+            $mergedRules =
+                array_merge_recursive(static::$rules['everytime'],
+                    static::$rules
+                    ['update']);
+        else
+            $mergedRules =
+                array_merge_recursive(static::$rules['everytime'],
+                    static::$rules
+                    ['create']);
+        $finalRules = [];
+        foreach ($mergedRules as $field => $rules) {
+            if (is_array($rules))
+                $finalRules[$field] = implode("|", $rules);
+            else
+                $finalRules[$field] = $rules;
+        }
+        return $finalRules;
     }
 
     private function autoHydrate(array $attributes)
